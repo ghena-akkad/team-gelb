83f0b19006b9cd71abadafe028ec422e6ac1fc78
Akkad <m_akkad23@cs.uni-kl.de>
2023-11-24 19:40:34 +0100

    rebasing

---
+++ EiOderZwei/src/main/java/com/example/eioderzwei/ShowImage.java
@@ -0,0 +1,46 @@
+package com.example.eioderzwei;
+
+import javafx.scene.image.Image;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Dies ist die Klasse, die jeden Kartennamen mit seinem Bild verknüpft.
+ */
+public enum ShowImage {
+    DUMMY_CARD("dummyCard", "/Users/akkad/Downloads/jpg123.png");
+    private String cardName;
+    private Image cardImage;
+
+    ShowImage(String cardName, String imageLoc) {
+        this.cardName = cardName;
+        try {
+            this.cardImage = new Image(new FileInputStream(imageLoc));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); // Handle the exception according to game logic later on
+        }
+    }
+    public String getCardName() {
+        return cardName;
+    }
+    public Image getImage() {
+        return cardImage;
+    }
+    public static String readCardName(Image image) {
+        for (ShowImage s: ShowImage.values()) {
+            if (s.getImage().equals(image)) {
+                return s.getCardName();
+            }
+        }
+        return null;
+    }
+    public static Image findImage(String cardName) {
+        for (ShowImage s : ShowImage.values()) {
+            if (s.getCardName().equals(cardName)) {
+                return s.getImage();
+            }
+        }
+        return DUMMY_CARD.getImage(); // Return the dummy card image if not found
+    }
+}
--- EiOderZwei/src/main/java/module-info.java
+++ EiOderZwei/src/main/java/module-info.java
@@ -4,8 +4,14 @@ module com.example.eioderzwei {
     requires javafx.web;
     requires java.rmi;
 
-
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
     //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires annotations;
 
     opens com.example.eioderzwei to javafx.fxml;
     exports com.example.eioderzwei;
